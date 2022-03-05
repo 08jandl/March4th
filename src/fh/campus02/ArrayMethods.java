54c60314b4675938436d514d2b263b54bbc664ae
Silke Jandl <silke8421@gmx.at>
2022-03-05 10:49:56 +0100

    correct looping over array + explanations

--- src/fh/campus02/ArrayMethods.java
+++ src/fh/campus02/ArrayMethods.java
@@ -6,6 +6,7 @@ public class ArrayMethods {
 
         printArray(schachtel);
         System.out.println(containsNumber(3, schachtel));
+        System.out.println(containsNumberWrong(17, schachtel));
     }
 
     public static void printArray(int[] array) {
@@ -18,16 +19,35 @@ public class ArrayMethods {
         }
     }
 
-    public static boolean containsNumber(int number, int[] array) {
+    public static boolean containsNumberWrong(int number, int[] array) {
         boolean isContained = false;
         for(int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
             if (array[i] == number) {
                 isContained = true;
             } else {
                 isContained = false;
             }
         }
+        //the reason this returns false for 3
+        // is that we only return isContained once we have gone through the whole array
+        //that means: we change isContained from false to true, to false whenever we look at the next item in the array
+        //so this way, true only comes back if n is actually the last item contained in the array
         return isContained;
+    }
+
+    // Here is how it works:
+    // We need to return true as soon as we find an item contained in the array
+    // the return statement marks the end of the operation
+    //that means: as soon as the code reaches a return, it breaks out of the loop
+    public static boolean containsNumber(int number, int[] array) {
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+            if (array[i] == number) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 }
